96e0e55180bca5f3a2a31bf8c5064bbcf768e821
Aleksandr Yefremov <yefremov.sasha@gmail.com>
2018-02-20 14:07:31 +0200

    Create back-to-top-button.js

---
+++ js/back-to-top-button.js
@@ -0,0 +1,39 @@
+(function () {
+  var scroll;
+  var scrollTime;
+  var startTime;
+
+  window.addEventListener('click', function (event) {
+    if (startTime) {
+      return;
+    }
+    // Scan for parent "button" element since it can contain other child elements, like svg in our case
+    var target = event.target;
+    do {
+      scrollTime = target.getAttribute('data-scrollupbutton-time');
+      if (isNumeric(scrollTime)) {
+        scroll = window.pageYOffset;
+        requestAnimationFrame(scroller);
+        break;
+      }
+    } while (target !== this && (target = target.parentElement)) // Don't scan higher than element to which the listener is assigned and not higher than <html>
+  }, false);
+
+  function scroller (timestamp) {
+    startTime || (startTime = timestamp);
+    timestamp = (timestamp - startTime) / +scrollTime;
+
+    if (!window.pageYOffset || timestamp >= 1) {
+      window.scrollTo(0, 0);
+      startTime = 0;
+      return;
+    }
+
+    window.scrollTo(0, scroll * (1 - 0.5 * (1 - Math.cos(Math.PI * timestamp))));
+    requestAnimationFrame(scroller);
+  }
+
+  function isNumeric (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+})();
